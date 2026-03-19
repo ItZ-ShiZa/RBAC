308dac3fd41fd68f78b10aadbcceead8d607592c
SARMATS <sora.nai.empty@gmail.com>
2026-03-20 00:00:13 +0700

    not a commit

---
+++ src/main/java/filters/AssignmentFilter.java
@@ -0,0 +1,16 @@
+package rbac.filters;
+
+import model.RoleAssignment;
+
+@FunctionalInterface
+public interface AssignmentFilter {
+    boolean test(RoleAssignment assignment);
+
+    default AssignmentFilter and(AssignmentFilter other) {
+        return a -> this.test(a) && other.test(a);
+    }
+
+    default AssignmentFilter or(AssignmentFilter other) {
+        return a -> this.test(a) || other.test(a);
+    }
+}
---
+++ src/main/java/filters/AssignmentSorters.java
@@ -0,0 +1,23 @@
+package rbac.filters;
+
+import model.RoleAssignment;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+
+public class AssignmentSorters {
+
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static Comparator<RoleAssignment> byUsername() {
+        return Comparator.comparing(a -> a.user().username());
+    }
+
+    public static Comparator<RoleAssignment> byRoleName() {
+        return Comparator.comparing(a -> a.role().getName());
+    }
+
+    public static Comparator<RoleAssignment> byAssignmentDate() {
+        return Comparator.comparing(a -> LocalDateTime.parse(a.metadata().assignedAt(), FORMAT));
+    }
+}
